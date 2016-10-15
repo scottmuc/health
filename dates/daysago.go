c91607e419a13b27252bf03100b1e38254d01397
Scott Muc <scottmuc@gmail.com>
2016-10-15 09:03:47 +0100

    Fix typo

--- dates/daysago.go
+++ dates/daysago.go
@@ -4,7 +4,7 @@ import "time"
 
 // DayDifference returns the number of days ago it is between two dates. The actual amount of time
 // doesn't really matter, it's the calendar date that this function uses. So if you have an activity
-// at 11:59pm yesterday, and current time is the next day at 1 minute after minute, this function will
+// at 11:59pm yesterday, and current time is the next day at 1 minute after midnight, this function will
 // return 1 because it happened a day ago.
 func DayDifference(then time.Time, now time.Time) int {
 	normalizedThen := time.Date(then.Year(), then.Month(), then.Day(), 23, 59, 59, 0, time.UTC)
